d55d9e73d3e5d9c137673c259c17706094578661
waytonoway <luckylifek@gmail.com>
2019-06-05 15:48:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,11 @@ export AWS_DEFAULT_REGION=$REGION
 
 if [[ "$1" == 'now' ]]; then
     exec /sync.sh
+else if [[ "$1" == 'backward-now' ]]; then
+    exec /sync_backward.sh
+else if [[ "$1" == 'backward' ]]; then
+    echo "$CRON_SCHEDULE /_backward.sh" >> /var/spool/cron/crontabs/rootexec
+    crond -l 2 -f
 else
 	echo "$CRON_SCHEDULE /sync.sh" >> /var/spool/cron/crontabs/root
     crond -l 2 -f
